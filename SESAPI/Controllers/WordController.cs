b275ca501f12452ce63e0dbc50bebd13796d8a94
Peter <67675215+ppap2711@users.noreply.github.com>
2021-05-25 10:10:32 -0500

    Add files via upload

---
+++ SESAPI/Controllers/WordController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SESAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetWord()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new {id=1, Name="Hello"},
+                    new {id=2, Name="test"}
+                });
+        }
+
+    }
+}
+
+/*
+ [HttpGet]
+        public JsonResult GetWord()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new {id=1, Name="Hello"},
+                    new {id=2, Name="test"}
+                });
+        }
+ */
